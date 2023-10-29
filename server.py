e99d86f7f9010cd3defa8904a1865937fac408ca
Anna Carolinne <70245734+anna-albuquerque@users.noreply.github.com>
2023-10-28 22:41:42 -0300

    teste pedra pepel e tesoura

--- server.py
+++ server.py
@@ -1,22 +1,27 @@
 import Pyro4
+import random
 
-@Pyro4.expose
-class TextServer(object):
+# Definindo a classe do jogo de Pedra, Papel e Tesoura
+class RPSGame(object):
     def __init__(self):
-        self.text = ""
+        self.choices = ["pedra", "papel", "tesoura"]
 
-    def send_text(self, text):
-        self.text = text
-        return "Texto atualizado com sucesso."
+    def play(self, player_choice):
+        server_choice = random.choice(self.choices)
+        if player_choice not in self.choices:
+            return "Escolha inválida. Escolha entre pedra, papel ou tesoura."
+        if player_choice == server_choice:
+            return f"Empate! Ambos escolheram {player_choice}."
+        if (player_choice == "pedra" and server_choice == "tesoura") or \
+           (player_choice == "papel" and server_choice == "pedra") or \
+           (player_choice == "tesoura" and server_choice == "papel"):
+            return f"Você ganhou! Você escolheu {player_choice}, e o servidor escolheu {server_choice}."
+        else:
+            return f"Você perdeu! Você escolheu {player_choice}, e o servidor escolheu {server_choice}."
 
-    def get_text(self):
-        return self.text
-
+# Inicializando o servidor Pyro
 daemon = Pyro4.Daemon()
-ns = Pyro4.locateNS()
-
-uri = daemon.register(TextServer)
-ns.register("text.server", uri)
+uri = daemon.register(RPSGame)
 
 print("Aguardando conexões...")
 daemon.requestLoop()
